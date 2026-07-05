55e0b5e3f584c5221a23a72f16d4e58d435360ea
Sandeep Ashok Reddy Patil <patilsandeepreddy1@gmailexit()exit()exit()git config --global user.email patilsandeepreddy1@gmail.com>
2026-07-05 10:43:12 +0530

    My first Python Program

---
+++ first.py
@@ -0,0 +1,13 @@
+print ('''My Name Is Sandeep Ashok Reddy Patil''')
+import math
+print (math.sqrt(144))
+print (math.pow(5,3))
+# print (math.sum(466+6493+64949))
+# print (math.max(4883*648*64664))
+# print (math.min(844545459-64488-6548)
+print (math.pi)
+
+
+
+
+
